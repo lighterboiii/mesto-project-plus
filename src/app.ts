486f8929c54e2b90aac1e83c9d1eada7962a79e2
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2023-06-17 15:00:55 +0300

    добавить роуты и контроллеры для пользователя

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/users";
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/users', userRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Всем привет, я джокер Виктор Дудка");
---
+++ src/routes/users.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { getUsers, getUserById, createUser } from "../controllers/users";
+
+const userRouter = Router();
+
+userRouter.get('/', getUsers);
+userRouter.get('/:userId', getUserById);
+userRouter.post('/', createUser);
+
+export default userRouter;
